f448b1d615717ec12bb8025bdfec0e76370f99d8
tranduong1995 <133027361+tranduong1995@users.noreply.github.com>
2023-06-08 23:53:21 +0700

    btvn4

---
+++ ngay_05/bttl1.java
@@ -0,0 +1,16 @@
+package ngay_05;
+
+public class bttl1 {
+     public static void main(String[] args) {
+
+          for (int i = 1; i <= 5; i++) {
+               for ( int j = 1; j <= i ; j++) {
+                    System.out.print(j);
+
+               }
+
+               System.out.println();
+          }
+
+     }
+}
---
+++ ngay_05/bttl2.java
@@ -0,0 +1,14 @@
+package ngay_05;
+
+public class bttl2 {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <= 5 ; i++ ) {
+            for (int j = 1 ; j <=i ; j++ ) {
+                System.out.print(i);
+            }
+             System.out.println();
+
+        }
+    }
+
+}
