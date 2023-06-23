b0e961d9970441e580a219c4705f0303214828ab
tranduong1995 <133027361+tranduong1995@users.noreply.github.com>
2023-06-23 13:02:03 +0700

    fddff

---
+++ javabtvn10/bai7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class bai7 {
+    public static void main(String[] args){
+        System.out.println("Nhập xâu s1 :");
+        System.out.println("Nhập xâu s2 :");
+        Scanner scanner = new Scanner(System.in);
+
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+
+
+    }
+}
